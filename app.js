6e96d9f466e7af3d6f4fab1faad93c3c8a58ceca
ANDRE LUIZ FRANCA BATISTA <01411769627>
2022-10-05 15:02:03 -0300

    Fixing the port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.port
 
 app.get('/', (req, res) => {
     res.send("Hello World2");

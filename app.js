0e1f99931be06ad3a242cb9862f595a5f5bdeba6
ANDRE LUIZ FRANCA BATISTA <01411769627>
2022-10-05 14:33:41 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("Hello World2");
+});
+
+app.get('/:animal/:id', (req, res) => {
+    res.send(`Você escolheu ${req.params.animal} com o número ${req.params.id}`)
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
